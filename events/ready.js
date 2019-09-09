46492eaaf24ad25f144aee4dceb4e2d7a3fb9473
LittleWhole <wholelittle@gmail.com>
2019-09-09 20:06:28 +0800

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async client => {
   client.logger.log(`${client.user.tag}\n${client.users.size} users \n${client.guilds.size} servers.`, "ready");
 
-  client.user.setActivity(`${client.config.prefix}help`, {type: "PLAYING"});
+  client.user.setActivity(`for ${client.config.prefix}help`, {type: "WATCHING"});
 };
