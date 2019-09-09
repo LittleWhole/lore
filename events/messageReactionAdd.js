a02a244b28248f9c372436fcec2949cac99d34ff
LittleWhole <wholelittle@gmail.com>
2019-09-09 19:04:55 +0800

    Create messageReactionAdd.js

---
+++ events/messageReactionAdd.js
@@ -0,0 +1,8 @@
+/* MESSAGE_REACTION_ADD Event
+   For role menus. */
+   
+module.exports = async (client, messageReaction, user) => {
+  // Debug. Will be removed when actual content is added to this event.
+  console.log(messageReaction);
+  console.log(user);
+}
